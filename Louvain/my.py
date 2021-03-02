a5949b73e5cfdb739f280cfc97700fa14381e6a4
XuHelin <xhl19@mails.tsinghua.edu.cn>
2021-03-02 19:50:17 +0800

    删了没用的代码

--- Louvain/my.py
+++ Louvain/my.py
@@ -394,51 +394,6 @@ def __randomize(items, random_state):
     return randomized_items
 
 
-def build_graph():
-    # just for test
-    G = nx.Graph()
-    f = open("demofile.txt", "r")
-    dot_num = int(f.readline())
-    for i in range(dot_num):  # 添加顶点
-        # print(f.readline())
-        G.add_node(int(f.readline()))
-    for line in f.readlines():  # 处理联结边
-        # print(line)
-        list = line.split()
-        G.add_edge(int(list[0]), int(list[1]), weight=float(list[2]))
-        # print(int(list[0]), int(list[1]), float(list[2]))
-        # print(float(G.degree(1, weight='weight')))
-    return G
-
-
-def hsv2rgb(h, s, v):
-    h = float(h)
-    s = float(s)
-    v = float(v)
-    h60 = h / 60.0
-    h60f = math.floor(h60)
-    hi = int(h60f) % 6
-    f = h60 - h60f
-    p = v * (1 - s)
-    q = v * (1 - f * s)
-    t = v * (1 - (1 - f) * s)
-    r, g, b = 0, 0, 0
-    if hi == 0:
-        r, g, b = v, t, p
-    elif hi == 1:
-        r, g, b = q, v, p
-    elif hi == 2:
-        r, g, b = p, v, t
-    elif hi == 3:
-        r, g, b = p, q, v
-    elif hi == 4:
-        r, g, b = t, p, v
-    elif hi == 5:
-        r, g, b = v, p, q
-    r, g, b = int(r * 255), int(g * 255), int(b * 255)
-    return r, g, b
-
-
 def get_color(idx, maxIdx):  # 随便搞了个哈希
     r = math.cos((2.0 / 3.0 * math.pi + 179.13 * idx / (maxIdx + 0.12))) * 256
     g = math.cos((2.0 / 3.0 * math.pi + 101.19 * idx / (maxIdx + 0.57))) * 256
